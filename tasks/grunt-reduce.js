d1721c023b67a884e7d80be7a9c551373d0b0429
Aleksei Zbiniakov <alexey@coccoc.com>
2017-05-23 17:53:01 +0700

    propogate all options to assetgraph-builder

--- tasks/grunt-reduce.js
+++ tasks/grunt-reduce.js
@@ -8,6 +8,34 @@
  * Licensed under the MIT license.
  */
 
+/********************/
+/* polifil Object.assign */
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    'use strict';
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+/********************/
+
 module.exports = function (grunt) {
 
     grunt.registerTask('reduce', 'Description', function () {
@@ -79,7 +107,7 @@ module.exports = function (grunt) {
             .logEvents()
             .registerRequireJsConfig()
             .loadAssets(loadAssets)
-            .buildProduction({
+            .buildProduction(Object.assign({
                 angular: config.angular,
                 recursive: true,
                 canonicalUrl: canonicalUrl,
@@ -98,7 +126,7 @@ module.exports = function (grunt) {
                 sharedBundles: sharedBundles,
                 stripDebug: !(config.pretty || false),
                 localeIds: localeIds
-            })
+            }, config))
             .writeAssetsToDisc({url: /^file:/, isLoaded: true}, outRoot)
             .if(cdnRoot)
                 .writeAssetsToDisc({url: query.createPrefixMatcher(cdnRoot), isLoaded: true}, cdnOutRoot || outRoot, cdnRoot)
